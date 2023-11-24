9d67e6489e5fed10795cd9093aa148864ca4561d
Landon Gray <landon.gray@testdouble.com>
2023-11-24 15:48:35 -0500

    bundle gem react_ai

---
+++ lib/react_ai.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "react_ai/version"
+
+module ReactAi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/react_ai/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ReactAi
+  VERSION = "0.1.0"
+end
